4e5ff3f8690dda5d5e4163795ecd13573bf797a6
Oleksandr Pavloshchuk <ap730518@gmail.com>
2018-11-13 14:10:50 +0200

    more tests

--- src/test/java/temp/ratio/calc/latex/Polynom2LaTeXUnitTest.java
+++ src/test/java/temp/ratio/calc/latex/Polynom2LaTeXUnitTest.java
@@ -27,5 +27,30 @@ public class Polynom2LaTeXUnitTest {
         Assert.assertEquals("{a}{b}^{2}-{a}^{2}{b}", sb.toString());
     }
 
+    @Test
+    public void newtonFormula() {
+        Polynom p = p();
+        for( int i=1; i<4; i++ ) {
+            p = Polynom.mul(p, p());
+        }
+        StringBuilder sb = new StringBuilder();
+        new Polynom2LaTeX(p).render(sb);
+
+        Assert.assertEquals("{a}^{4}-4{a}^{3}{b}+6{a}^{2}{b}^{2}-4{a}{b}^{3}+{b}^{4}", sb.toString());
+    }
+
+    static Polynom p() {
+        Nom a = new Nom();
+        a.append("a", 1);
+
+        Nom b = new Nom();
+        b.append("b", 1);
+
+        Polynom p = new Polynom();
+        p.add(a, 1);
+        p.add(b, -1);
+        return p;
+    }
+
     
 }
